1af2c38212ae149d823862e3567092acb92fbca5
tarkesh2shar <tarkesh2shar@gmail.com>
2021-01-06 00:20:52 +0530

    just few updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-
+const express = require("express"); 
+const path=require("path");
 const app = express();
 
 const port = process.env.PORT || 2000;
@@ -12,3 +12,12 @@ app.use(express.static("client/dist"));
 app.get("/server/products", (req, res) => {
   res.send({ data: "Sending something to the server here" });
 });
+if(process.env.NODE_ENV==="production"){
+
+  const path=require('path');
+
+  app.get('/*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./client","dist","index.html"))
+  })
+
+}
